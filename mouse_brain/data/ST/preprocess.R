8687287043d7dffabb9c28a1856b511566736de9
TianyouLuo <tianyou@live.unc.edu>
2021-09-09 14:24:11 -0400

    Update preprocess.R for easier slice change

--- mouse_brain/data/ST/preprocess.R
+++ mouse_brain/data/ST/preprocess.R
@@ -1,13 +1,15 @@
 library(data.table)
-count = fread("/pine/scr/t/i/tianyou/ST/mouse_brain_cell2location/data/ST/ST8059048/raw_feature_bc_matrix/matrix.mtx.gz",header=F,fill=TRUE,skip=2)
+
+STdir = "/pine/scr/t/i/tianyou/ST/mouse_brain_cell2location/data/ST/ST8059052/"
+count = fread(file.path(STdir,"raw_feature_bc_matrix/matrix.mtx.gz"),header=F,fill=TRUE,skip=2)
 cnt = matrix(rep(0,count$V1[1]*count$V2[1]),nrow=count$V2[1])
 for(i in 2:nrow(count)){
   cnt[count$V2[i],count$V1[i]]=count$V3[i]
 }
-gene = fread("features.tsv.gz",header=F)
-barcode = fread("barcodes.tsv.gz",header=F)
-annotation = fread("/pine/scr/t/i/tianyou/ST/mouse_brain_cell2location/data/ST/ST8059048/annotation/ST8059048_markers_layers.csv")
-annotation = annotation[annotation$ST8059048 != ""]
+gene = fread(file.path(STdir, "raw_feature_bc_matrix/features.tsv.gz"),header=F)
+barcode = fread(file.path(STdir, "raw_feature_bc_matrix/barcodes.tsv.gz"),header=F)
+annotation = fread(file.path(STdir, "annotation/ST8059052_markers_layers.csv"))
+annotation = annotation[annotation$ST8059052 != ""]
 duplicate_gene = gene$V2[which(duplicated(gene$V2))]
 sum(duplicated(gene$V2))
 
@@ -28,4 +30,4 @@ gene_present = apply(cnt, 2, function(x){sum(x>0)})
 cnt = cnt[,gene_present > 0.02 * dim(cnt)[1]]
 cnt_t = t(cnt)
 
-write.table(cnt_t,"/pine/scr/t/i/tianyou/ST/mouse_brain_cell2location/data/ST/ST8059048/ST8059048_processed.tsv",row.names=T,col.names=T,sep="\t",quote=F)
+write.table(cnt_t, file.path(STdir, "ST8059052_processed.tsv"),row.names=T,col.names=T,sep="\t",quote=F)
