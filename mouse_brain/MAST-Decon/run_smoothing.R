0df0755c474279d90a0c3e1ca4e55a28c917b022
TianyouLuo <tianyou@live.unc.edu>
2021-09-11 14:19:39 -0400

    Add subsampling ST data codes

--- mouse_brain/MAST-Decon/run_smoothing.R
+++ mouse_brain/MAST-Decon/run_smoothing.R
@@ -25,89 +25,18 @@ results = train.smooth(spatialRNA = spatialRNA, theta0 = w, UMI = UMI,
                        c = 1, radius_seq = c(221, 382, 442), tol = 10^-6, trace = 0)
 
 results_tibb = as_tibble(results, rownames = "barcodes")
-#write_csv(results_tibb, "smoothing_medC.csv")
-
-results_norm = results_tibb
-sum_celltype = apply(results_tibb[,-1], 1, sum)
-results_norm[,-1] = sweep(results_norm[,-1], 1, sum_celltype, "/")
-#write_csv(results_norm_tibb, "smoothing_norm_lowC.csv")
-results_norm_loc = results_norm %>%
-  left_join(coords, by = "barcodes") %>%
-  select(barcodes, xcoord, ycoord, eval(numeric_celltype$assigned_cluster))
-write_csv(results_norm_loc, "smoothing_medC_norm.csv")
-
-ggplot() +
-  geom_scatterpie(aes(x = ycoord, y = xcoord), data = results_norm_loc, 
-                  cols=numeric_celltype$assigned_cluster, pie_scale = 0.8) +
-  coord_equal() +
-  ggsave("smooth_medC.png")
-
-
-##### low smoothing ###
-results = train.smooth(spatialRNA = spatialRNA, theta0 = w, UMI = UMI, 
-                       cell_type_mean_norm = cell_type_mean_norm, distmat = distmat,
-                       r0 = 62, c = 0.5, s = 1.42, iter = 3, tol = 10^-6, trace = 0)
-
-results_tibb = as_tibble(results, rownames = "barcodes")
+write_csv(results_tibb, file.path(resultdir, "smoothing_medC.csv"))
 
 results_norm = results_tibb
 sum_celltype = apply(results_tibb[,-1], 1, sum)
 results_norm[,-1] = sweep(results_norm[,-1], 1, sum_celltype, "/")
 results_norm_loc = results_norm %>%
-  left_join(coords, by = "barcodes") %>%
-  select(barcodes, xcoord, ycoord, eval(numeric_celltype$assigned_cluster))
-write_csv(results_norm_loc, "smoothing_lowC_norm.csv")
+  left_join(as_tibble(loc, rownames="barcodes"), by = "barcodes") %>%
+  rename(xcoord = x, ycoord = y)
+celltype_names = colnames(results_tibb)[-1]
 
 ggplot() +
-  geom_scatterpie(aes(x = ycoord, y = xcoord), data = results_norm_loc, 
-                  cols=numeric_celltype$assigned_cluster, pie_scale = 0.8) +
+  geom_scatterpie(aes(x = ycoord, y = 10000-xcoord), data = results_norm_loc,
+                  cols=celltype_names, pie_scale = 0.5) +
   coord_equal() +
-  ggsave("smooth_lowC.png")
-
-
-##### high smoothing ###
-results = train.smooth(spatialRNA = spatialRNA, theta0 = w, UMI = UMI, 
-                       cell_type_mean_norm = cell_type_mean_norm, distmat = distmat,
-                       r0 = 62, c = 2, s = 1.42, iter = 3, tol = 10^-6, trace = 0)
-
-results_tibb = as_tibble(results, rownames = "barcodes")
-
-results_norm = results_tibb
-sum_celltype = apply(results_tibb[,-1], 1, sum)
-results_norm[,-1] = sweep(results_norm[,-1], 1, sum_celltype, "/")
-results_norm_loc = results_norm %>%
-  left_join(coords, by = "barcodes") %>%
-  select(barcodes, xcoord, ycoord, eval(numeric_celltype$assigned_cluster))
-write_csv(results_norm_loc, "smoothing_highC_norm.csv")
-
-ggplot() +
-  geom_scatterpie(aes(x = ycoord, y = xcoord), data = results_norm_loc, 
-                  cols=numeric_celltype$assigned_cluster, pie_scale = 0.8) +
-  coord_equal() +
-  ggsave("smooth_highC.png")
-
-
-
-
-
-
-########### draw original RCTD results ########
-RCTD_result = w
-
-sum_celltype = apply(RCTD_result, 1, sum)
-RCTD_results_norm = sweep(RCTD_result, 1, sum_celltype, "/")
-RCTD_results_norm_tibb = as_tibble(RCTD_results_norm)
-RCTD_results_norm_tibb$loc = rownames(RCTD_results_norm)
-RCTD_results_norm_tibb = RCTD_results_norm_tibb %>% select(loc, everything())
-#write_csv(RCTD_results_norm_tibb, "RCTD_results.csv")
-
-RCTD_results_full = coords %>%
-  right_join(RCTD_results_norm_tibb, by=c("barcodes"="loc"))
-#cell_type_names = colnames(RCTD_results_full)[5:13]
-
-ggplot() +
-  geom_scatterpie(aes(x = ycoord, y = xcoord), data = RCTD_results_full, 
-                  cols=celltypes, pie_scale = 0.8) +
-  coord_equal() +
-  ggsave("RCTD_result.png")
-
+  ggsave(file.path(resultdir, "smoothing_medC.png"), width = 12, height = 8)
---
+++ mouse_brain/data/ST/subsampling/subsampling.R
@@ -0,0 +1,45 @@
+library(data.table)
+
+STdir = "/pine/scr/t/i/tianyou/ST/mouse_brain_cell2location/data/ST/ST8059048/"
+ST_orig = fread(file.path(STdir, "ST8059048_processed.tsv"))
+
+## function to subsample to a read depth for one spot
+subsample.spot = function(ST, depth){
+  prob_vec = ST / sum(ST)
+  reads = rmultinom(1, depth, prob_vec)
+  return(reads)
+}
+
+subsample.ST = function(STdat, depth = 100000, sd = 5000){
+  subsampled_mat = STdat
+  depth_vec = round(rnorm(dim(STdat)[2]-1, depth, sd))
+  for (i in 2:dim(STdat)[2]){
+    vec = STdat[,..i][[1]] ##..used for data.table
+    subsample_vec = subsample.spot(vec, depth_vec[i-1])
+    subsampled_mat[,i] = subsample_vec
+  }
+  return(subsampled_mat)
+}
+
+process.ST = function(cnt){
+  gene_sum = apply(cnt[,2:dim(cnt)[2]], 1, sum)
+  cnt = cnt[gene_sum!=0,]
+  
+  gene_present = apply(cnt[,2:dim(cnt)[2]], 1, function(x){sum(x>0)})
+  cnt = cnt[gene_present > 0.02 * (dim(cnt)[2]-1),]
+}
+
+ST_sub2500 = subsample.ST(ST_orig, depth = 2500, sd = 500)
+ST_sub2500_process = process.ST(ST_sub2500)
+write.table(ST_sub2500_process, file.path(STdir, "ST8059048_sub2500_processed.tsv"),
+            row.names=F,col.names=T,sep="\t",quote=F)
+
+ST_sub3500 = subsample.ST(ST_orig, depth = 3500, sd = 500)
+ST_sub3500_process = process.ST(ST_sub3500)
+write.table(ST_sub3500_process, file.path(STdir, "ST8059048_sub3500_processed.tsv"),
+            row.names=F,col.names=T,sep="\t",quote=F)
+
+ST_sub1500 = subsample.ST(ST_orig, depth = 1500, sd = 500)
+ST_sub1500_process = process.ST(ST_sub1500)
+write.table(ST_sub1500_process, file.path(STdir, "ST8059048_sub1500_processed.tsv"),
+            row.names=F,col.names=T,sep="\t",quote=F)
